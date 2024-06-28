a0a97da5aed9f54c5e54375630d72784b48edce2
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-06-28 16:04:01 +0300

    merging build.cs file - it still has 2 private dependies to fix

--- Source/Anchorpoint/Anchorpoint.Build.cs
+++ Source/Anchorpoint/Anchorpoint.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Some copyright should be here...
 
 using System;
 using System.IO;
@@ -8,80 +8,49 @@ public class Anchorpoint : ModuleRules
 {
 	public Anchorpoint(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-        
-        PublicDefinitions.AddRange(
-            new string[] {
-                "OUTCOME_ENABLE_CXX_MODULES=0",
+		PrivateDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"Slate",
+			"SlateCore", 
+			"SourceControl"
+		});
+
+		PublicDefinitions.AddRange(
+			new string[] {
+				"OUTCOME_ENABLE_CXX_MODULES=0",
 				"OUTCOME_FORCE_LEGACY_GCC_CXX_CONCEPTS=0"
-            }
-            );
-        
-		if (Target.Platform == UnrealTargetPlatform.Win64) {
-			PublicAdditionalLibraries.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-					Path.Combine(ModuleDirectory, "apsync", "win", "sync.lib")
-				}
-            );
+			}
+			);
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "apsync", "win", "sync.lib"));
 			RuntimeDependencies.Add(Path.Combine(ModuleDirectory, "apsync", "win", "sync.dll"));
 		}
-		else if (Target.Platform == UnrealTargetPlatform.Mac) {
-			PublicAdditionalLibraries.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-					Path.Combine(ModuleDirectory, "apsync", "mac", "libsync.dylib")
-				}
-            );
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "apsync", "mac", "libsync.dylib"));
 			RuntimeDependencies.Add(Path.Combine(ModuleDirectory, "apsync", "mac", "libsync.dylib"));
 		}
-        
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-                Path.Combine(ModuleDirectory, "apsync", "include")
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
+
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "apsync", "include"));
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Projects",
-                "InputCore",
-                "EditorFramework",
-                "UnrealEd",
-                "ToolMenus",
-                "CoreUObject",
-                "Engine",
-                "Slate",
-                "SlateCore",
-                "Json",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
+				"InputCore",
+				"EditorFramework",
+				"UnrealEd",
+				"ToolMenus",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"Json",
 			}
 			);
 	}
