d3907d3b119d4e81a70c897f83d5788677ffaf96
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-08-24 16:50:44 +0300

    implemented apsync logger interface

---
+++ Source/AnchorpointSync/Public/AnchorpointLogger.h
@@ -0,0 +1,34 @@
+﻿// Some copyright should be here...
+
+#pragma once
+
+#include "apsync/service/logger.h"
+
+class ANCHORPOINTSYNC_API FAnchorpointLog final : public apsync::ILog
+{
+public:
+	explicit FAnchorpointLog(apsync::LogLevel InLevel);
+
+private:
+	// Begin apsync::ILog interface
+	virtual void appendLog(const std::string& data) override;
+	virtual void endline() override;
+	// End apsync::ILog interface
+
+	apsync::LogLevel LogLevel = apsync::LogLevel::Debug;
+};
+
+/**
+ * Implementation of the apsync::ILogger responsible for forwarding logs to Unreal's system 
+ */
+class ANCHORPOINTSYNC_API FAnchorpointLogger final : public apsync::ILogger
+{
+public:
+	virtual ~FAnchorpointLogger() = default;
+
+private:
+	// Begin apsync::ILogger interface
+	virtual void log(apsync::LogLevel level, std::string& msg) override;
+	virtual apsync::LogHandle getLogStream(apsync::LogLevel level) override;
+	// End apsync::ILogger interface
+};
